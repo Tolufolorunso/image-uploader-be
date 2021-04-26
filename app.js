558b6b4572f1c3749380251c2e98a3cad761ca27
KolaTolulope <81151811+KolaTolulope@users.noreply.github.com>
2021-04-26 12:44:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const morgan = require('morgan')
+const colors = require('colors')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+
+const imageRouter = require('./route/image')
+
+//load env var
+dotenv.config({
+  path: './config/config.env',
+})
+
+const app = express()
+app.use(cors())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+})
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    // tempFilesDir: '/tmp/',
+  })
+)
+
+app.use('/', imageRouter)
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+  )
+)
---
+++ route/image.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const cloudinary = require('cloudinary').v2
+const path = require('path')
+
+const uploadImage = async (req, res) => {
+  const acceptedImage = ['jpg', 'jpeg', 'png']
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({ status: 'success', errorMsg: imageUrl })
+  }
+  // console.log(path.join(__dirname, '../', 'tmp'))
+  const file = req.files.image
+  // let fileToDelete = file.tempFilePath.split('\\')
+  // fileToDelete = fileToDelete[fileToDelete.length - 1]
+  // console.log('13', fileToDelete)
+  if (!acceptedImage.includes(file.mimetype.split('/')[1])) {
+    return res
+      .status(400)
+      .json({ status: 'fail', errorMsg: 'Accepted image format, jpeq,jpg,png' })
+  }
+  try {
+    const image = await cloudinary.uploader.upload(file.tempFilePath, {
+      width: 250,
+      height: 250,
+      crop: 'fill',
+      gravity: 'face',
+    })
+
+    const imageUrl = image.url.substr(5)
+    console.log('46', imageUrl)
+
+    res.status(200).json({ status: 'success', imageUrl })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ status: 'fail', errorMsg: 'Something went wrong' })
+  }
+  console.log(file)
+}
+
+const home = (req, res) => {
+  res.status(200).json({ status: 'success', message: 'Home' })
+}
+
+router.post('/api/v1/image', uploadImage)
+router.get('/', home)
+
+module.exports = router
